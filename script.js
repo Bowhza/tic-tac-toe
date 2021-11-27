573cf5faa5871e27725e5726859de9b82c8af1d9
Bowhza <bogtank2003@gmail.com>
2021-11-26 23:00:54 -0700

    Added a "play again" prompt.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const mainContainer = document.querySelector(".main-container");
+
 const player = (name, marker) => {
     const playerName = name;
     const playerMarker = marker;
@@ -9,6 +11,7 @@ const player2 = player("Player 2", "O");
 
 const gameBoard = (() => {
     const gridBoxes = Array.from(document.querySelectorAll(".grid-box"));
+    const eventText = document.getElementById("event-text");
     let _gameBoardArray = [];
     
     for (box = 0; box < gridBoxes.length; box++) {
@@ -17,56 +20,84 @@ const gameBoard = (() => {
 
     let currentTurn = player1.playerMarker;
     let currentPlayer = player1.playerName;
-    let gameOn = true
-
 
     gridBoxes.forEach(box => {
         box.addEventListener("click", () => {
-            if (gameOn){
-                if (box.textContent == "") {
-                    box.textContent = currentTurn;
-                    _gameBoardArray[box.id] = currentTurn;
-                    checkWinner();
-                    if (currentTurn == player1.playerMarker) {
-                        currentTurn = player2.playerMarker;
-                        currentPlayer = player2.playerName;
-                    } else {
-                        currentTurn = player1.playerMarker;
-                        currentPlayer = player1.playerName;
-                    };
+            if (box.textContent == "") {
+                box.textContent = currentTurn;
+                _gameBoardArray[box.id] = currentTurn;
+                checkWinner();
+                if (currentTurn == player1.playerMarker) {
+                    currentTurn = player2.playerMarker;
+                    eventText.textContent = `It is ${player2.playerName}'s turn.`;
+                    currentPlayer = player2.playerName;
+                } else {
+                    currentTurn = player1.playerMarker;
+                    eventText.textContent = `It is ${player1.playerName}'s turn.`;
+                    currentPlayer = player1.playerName;
                 };
             };
         });
     });
 
     const winConditions = [
-        [0, 1, 2],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0, 1, 2], [0, 3, 6], [1, 4, 7], [2, 5, 8], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]
     ];
 
+    let winner = "";
+    
     const checkWinner = () => {
         for (x = 0; x < winConditions.length; x++) {
             winCondiditon = winConditions[x];
-            console.log(winCondiditon);
-            let spot1 = _gameBoardArray[winCondiditon[0]]
-            let spot2 = _gameBoardArray[winCondiditon[1]]
-            let spot3 = _gameBoardArray[winCondiditon[2]]
-            console.log(spot1, spot2, spot3)
-            console.log(_gameBoardArray)
-            if (spot1 == "" && spot2 == "" && spot3 ==""){
+            let spot1 = _gameBoardArray[winCondiditon[0]];
+            let spot2 = _gameBoardArray[winCondiditon[1]];
+            let spot3 = _gameBoardArray[winCondiditon[2]];
+            if (spot1 == "" && spot2 == "" && spot3 == ""){
                 continue;
             }
             if (spot1 === spot2 && spot2 === spot3){
-                alert(`${currentPlayer} HAS WON THE GAME!`)
-                gameOn = false
+                winner = `${currentPlayer} has won the game!`;
+                playAgain();
             };
         };
+        if (!_gameBoardArray.includes("")) {
+            winner = "It's a Tie!";
+            playAgain();
+        };
     };
-    
+
+    const playAgain = () => {
+        const promptContainer = document.createElement("div");
+        promptContainer.classList.add("prompt-container");
+
+        const winnerText = document.createElement("div");
+        winnerText.textContent = `Game Over! ${winner}`;
+
+        const playAgainPromp = document.createElement("div");
+        playAgainPromp.classList.add("play-again");
+
+        const playAgainText = document.createElement("div");
+        playAgainText.innerText = "Would you like to play again?";
+
+        const playAgainButton = document.createElement("button");
+        playAgainButton.innerText = "Play Again?";
+        playAgainButton.id = "play-again-button";
+
+        promptContainer.append(playAgainPromp);
+        playAgainPromp.append(winnerText, playAgainText, playAgainButton);
+        mainContainer.appendChild(promptContainer);
+
+        playAgainButton.addEventListener("click", () => {
+            _gameBoardArray.length = 0;
+            for (box = 0; box < gridBoxes.length; box++) {
+                _gameBoardArray.push("");
+                gridBoxes.forEach(box => {
+                    box.textContent = "";
+                });
+            };
+            mainContainer.removeChild(promptContainer);
+        });
+        return {winnerText};
+    };
+
 })();
